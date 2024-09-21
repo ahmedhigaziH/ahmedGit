bcc7b0509e13b1218140e5b606ee66389883bfd3
ahmedhigaziH <ahmedhigaizh@gmail.com>
2024-09-21 17:18:38 +0300

    asa

--- app/src/main/java/com/example/ahmedgit/MainActivity.java
+++ app/src/main/java/com/example/ahmedgit/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ahmedgit;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,48 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txt;
+    private boolean flag = true;
+    private int num = 0;
+    private Thread count;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        txt = findViewById(R.id.Number);
     }
+
+    public void start(View view) {
+        count = new Thread(() -> {
+            try {
+                flag=true;
+                while (flag) {
+                    num++;
+                    txt.setText(num + "");
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+
+            }
+        });
+        count.start();
+
+    }
+
+    public void stop(View view) {
+        flag=false;
+        Thread.interrupted();
+    }
+
 }
+
+
+
+
+
+
+
+
+
